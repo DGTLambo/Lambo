a61532516ef2cd9188f99ad4bc69fb4e2419a12b
sudo-avoid-doxx <71901758+sudo-avoid-doxx@users.noreply.github.com>
2020-11-14 17:50:29 -0500

    push website v1

---
+++ precache-manifest.36b1194a3978374f015702ef539c0aa2.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5bbdc5f8194e505db119704ad5592b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b832fa56dbcb06ccfde",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "af485a32db685de7e95b",
+    "url": "/static/css/main.8d149e16.chunk.css"
+  },
+  {
+    "revision": "6b832fa56dbcb06ccfde",
+    "url": "/static/js/2.cc40ff3b.chunk.js"
+  },
+  {
+    "revision": "3b5bb7400a062057dc2040461b292a19",
+    "url": "/static/js/2.cc40ff3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af485a32db685de7e95b",
+    "url": "/static/js/main.f55edf71.chunk.js"
+  },
+  {
+    "revision": "2d7c1e7eb3492b57f9f8",
+    "url": "/static/js/runtime-main.bb0dd0db.js"
+  },
+  {
+    "revision": "5892adccd6c4b8c9e80f005e7eef06f2",
+    "url": "/static/media/AstroSpace-eZ2Bg.5892adcc.ttf"
+  },
+  {
+    "revision": "abaa020a82b199437857f5680e2c1d18",
+    "url": "/static/media/Lamb_OG.abaa020a.mp3"
+  },
+  {
+    "revision": "38b72c488975b8796c08bf0fb793b267",
+    "url": "/static/media/Lambo_Degens.38b72c48.mp3"
+  },
+  {
+    "revision": "16a1e4b47048e82c16ef70560b5418ce",
+    "url": "/static/media/Lambo_Token.16a1e4b4.mp3"
+  },
+  {
+    "revision": "92e433743c99e0051efa7c8e491863bb",
+    "url": "/static/media/Lambo_Token_2.0_1.92e43374.mp3"
+  },
+  {
+    "revision": "55f7da3380a0de97835bf8d1c71a2421",
+    "url": "/static/media/Lambo_ong.55f7da33.mp3"
+  },
+  {
+    "revision": "8ef10a4bb51821b078669ee032c28d54",
+    "url": "/static/media/My_lambo_Stash.8ef10a4b.mp3"
+  },
+  {
+    "revision": "79e8fa64a8503259d3ff45d06fbc01b8",
+    "url": "/static/media/clouds.79e8fa64.png"
+  },
+  {
+    "revision": "533b6d9160dda37719bba1f6c75c09b5",
+    "url": "/static/media/discord.533b6d91.svg"
+  },
+  {
+    "revision": "8776947257a412577ef6d219960b09a6",
+    "url": "/static/media/earth1.87769472.png"
+  },
+  {
+    "revision": "93d9c1ab4a833e8b96ea4916dccbb32c",
+    "url": "/static/media/earth2.93d9c1ab.png"
+  },
+  {
+    "revision": "4076b89dd300b3025ae86f64ac185e3c",
+    "url": "/static/media/lambo-icon-optimized.4076b89d.svg"
+  },
+  {
+    "revision": "bd78109fc53b3bde545a7a1f3dbc02a6",
+    "url": "/static/media/lambo-red.bd78109f.png"
+  },
+  {
+    "revision": "2dd12c57a74c9b227ff4bef0df61cb3f",
+    "url": "/static/media/lambo-sideview.2dd12c57.webp"
+  },
+  {
+    "revision": "68e7eeb6a0cb38031482461d69d15468",
+    "url": "/static/media/lambo-yellow.68e7eeb6.png"
+  },
+  {
+    "revision": "c839655d7a4a59c0add56dbd9784bfb9",
+    "url": "/static/media/lambomoon.c839655d.png"
+  },
+  {
+    "revision": "059afae3936745005352e397ebcfd7d3",
+    "url": "/static/media/lamborghini.059afae3.png"
+  },
+  {
+    "revision": "f1c256effced544a69018c4b72a6da66",
+    "url": "/static/media/medium.f1c256ef.svg"
+  },
+  {
+    "revision": "d717c4a19ef78225765c35b568052914",
+    "url": "/static/media/moon-footer-2.d717c4a1.png"
+  },
+  {
+    "revision": "dc5557970995508b2bda7d6000cdf9b9",
+    "url": "/static/media/moon-footer.dc555797.png"
+  },
+  {
+    "revision": "58bae3e6855c05449d4bd792021e797d",
+    "url": "/static/media/moon-surface.58bae3e6.png"
+  },
+  {
+    "revision": "38fce3ce1b41675a098f88309a369363",
+    "url": "/static/media/needle.38fce3ce.png"
+  },
+  {
+    "revision": "5cd638b6c2f701c56359381a37becf49",
+    "url": "/static/media/poster2.5cd638b6.jpg"
+  },
+  {
+    "revision": "7a111d5507e4d82ae2ebfc73fd3c0706",
+    "url": "/static/media/poster3.7a111d55.jpg"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/static/media/stars.6494ab5b.png"
+  },
+  {
+    "revision": "7bbc8f5259305c027cd5f3cfaa189265",
+    "url": "/static/media/telegram.7bbc8f52.svg"
+  },
+  {
+    "revision": "c94d34c77a9191b04d7603859adf6616",
+    "url": "/static/media/twap.c94d34c7.png"
+  },
+  {
+    "revision": "0729c42ac6c88ea12311c6989df785d6",
+    "url": "/static/media/twitter.0729c42a.svg"
+  },
+  {
+    "revision": "136fa14ffd939f41f9d211960fe65225",
+    "url": "/static/media/white-lambo.136fa14f.png"
+  }
+]);
